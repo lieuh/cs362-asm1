9d6c56b3c7927912fde6472e1f45cab07d2f11ba
Helena Lieu <lieuh@oregonstate.edu>
2024-04-08 14:49:36 -0700

    added figlet

--- asciiArt.js
+++ asciiArt.js
@@ -1,5 +1,15 @@
+const figlet = require('figlet');
+
 // Grab the first command line argument after the program name
 const userInput = process.argv[2];
 
-// Print the user input
-console.log(userInput);
+// Convert the user input to ASCII art using Figlet
+figlet(userInput, function(err, data) {
+    if (err) {
+        console.log('Something went wrong...');
+        console.dir(err);
+        return;
+    }
+    // Print the generated ASCII art
+    console.log(data);
+});
